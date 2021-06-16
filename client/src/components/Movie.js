5bb2c9252a0b90ef1388692ad0133b53953227b4
AdrianHDev <adrianhensondev@gmail.com>
2021-06-16 19:33:23 -0400

    Set up movie deletion

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -4,7 +4,7 @@ import { Link, useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 const Movie = (props) => {
-    const { addToFavorites } = props;
+    const { deleteMovie, addToFavorites } = props;
 
     const [movie, setMovie] = useState('');
 
@@ -21,6 +21,10 @@ const Movie = (props) => {
             })
     }, [id]);
 
+    const handleDelete = () => {
+        deleteMovie(id);
+        push('/movies')
+    }
     return(<div className="modal-page col">
         <div className="modal-dialog">
             <div className="modal-content">
@@ -52,7 +56,7 @@ const Movie = (props) => {
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
                             <Link to={`/movies/edit/${movie.id}`} className="m-2 btn btn-success">Edit</Link>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete"><input onClick={handleDelete} type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
                 </div>
